45f5bda31e2a8b7231831a41cdbb65e0989e392f
crislmfroes <cris.lima.froes@gmail.com>
2021-01-15 15:13:29 -0300

    added product category schema

--- models/Product.js
+++ models/Product.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
     title: String,
-    descruption: String,
+    description: String,
     price: Number,
-    category: String
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ProductCategory'
+    }
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
---
+++ routes/products.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const Product = require('../models/Product');
+
+const router = express.Router();
